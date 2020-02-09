a3a162640014a6fb4877da77483ef78f595b6ca5
Alexandra Nicolaides <alexandrajn17@gmail.com>
2020-02-09 20:08:20 +0000

    strikethrough & done button removed

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Task.css";
+import { thisExpression } from "@babel/types";
 
 class Task extends React.Component {
   deleteClicked = () => {
@@ -9,32 +10,41 @@ class Task extends React.Component {
   doneClicked = () => {
     // this.props.completedTaskFunc(this.props.item.id);
     this.props.completedTaskFunc(this.props.item.id);
-  }
+  };
 
   // doneClicked = () => {
   //     alert(`you clicked done button for task ${this.props.item.id}`)
   // }
 
   render() {
+
+    let description;
+
+    if(this.props.item.completed){
+      description = <div className="col-12 col-md-8 completedTask">{this.props.item.description}</div>
+    } else {
+      description = <div className="col-12 col-md-8">{this.props.item.description}</div>
+
+    }
+    
+
     return (
       <div className="row task">
-        <div className="col-12 col-md-8">{this.props.item.description}</div>
+        {description}
+        <div className="col-12 col-md-8"></div>
 
         <div className="col-3 col-md-2">
-          <button
-            type="button"
-            className="btn btn-success"
-            onClick={this.doneClicked}
-          >
+          {!this.props.item.completed && (              //if the task is not completed + the done button is clicked, remove the done button
+          <button type="button" className="btn btn-success" onClick={this.doneClicked}>
             Done
           </button>
+          )}
         </div>
         <div className="col-3 col-md-2">
           <button
             type="button"
             className="btn btn-danger"
-            onClick={this.deleteClicked}
-          >
+            onClick={this.deleteClicked} >
             Delete
           </button>
         </div>
