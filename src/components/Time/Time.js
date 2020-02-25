636c46c247629ee132d87a4ec9700ffa6c281f3a
Alexandra <alexandrajn17@gmail.com>
2020-02-25 19:56:42 +0000

    adding suffix to date with a switch statement

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function Time() {
     //split divides the date/time string at each space and returns an array
-    const today = (new Date()).toString().split(' ').splice(1, 2).join(' '); //splice(1, 2) takes the 2nd & 3rd values of the string
+    const today = (new Date()).toString().split(' ').splice(1, 2).join(' ') + addSuffix(); //splice(1, 2) takes the 2nd & 3rd values of the string
     //then we join those together using the .join() method
 
 
@@ -24,6 +24,25 @@ function Time() {
         const newTime = new Date().toLocaleTimeString(); //this will allow us to call a new updated time
         setTime(newTime); //this is the function we delcared would update our state in the destructured array
     }
+
+
+    function addSuffix(num) {
+        var array = ("" + num).split("").reverse(); // E.g. 123 = array["3","2","1"] - needs to be reversed because of single digit dates
+
+        if (array[1] !== "1") { // Number is 11th, 12th, 13th)
+
+            switch (array[0]) {    //get the first value of the array
+                case "1": return "st";
+                case "2": return "nd";
+                case "3": return "rd";
+                default:
+            }
+        }
+        return "th";  //else return 'th'
+    }
+
+    addSuffix(today);
+
 
     return (
         <div className="container">
