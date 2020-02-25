4e65976b780ac8657798a51e30aad32f08ed8385
Alexandra <alexandrajn17@gmail.com>
2020-02-25 19:21:28 +0000

    more notes added to Time.js, notes tidied

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 
 function Time() {
+    //split divides the date/time string at each space and returns an array
+    const today = (new Date()).toString().split(' ').splice(1, 2).join(' '); //splice(1, 2) takes the 2nd & 3rd values of the string
+    //then we join those together using the .join() method
 
 
-    const today = (new Date()).toString().split(' ').splice(1, 2).join(' ');
 
-
-
-    setInterval(updateTime, 1000); //call updatedTime every second
-
-    const now = new Date().toLocaleTimeString(); //setting now to the current time
-
+    const now = new Date().toLocaleTimeString(); //setting 'now' to the current time
     //below we set a destructured aray as a constant and assign the destructured array to useState
     //we now need to declare two things:
     //FIRST we pass in the name/state of this piece of state
@@ -18,6 +15,9 @@ function Time() {
     const [time, setTime] = useState(now);
     //starting value of state is defined in useState()'s parentheses
     //in this case it's the current time which we stored at the top of our App in the const 'now'
+    setInterval(updateTime, 1000); //calls updatedTime every second
+
+
 
     //this is the function that will update the inital value of the initial state
     function updateTime() {
@@ -28,8 +28,8 @@ function Time() {
     return (
         <div className="container">
 
-            <div className="row ml-auto time">
-                <span><h1 className="text-right">{today}</h1></span>
+            <div className="row ml-auto justify-content-right time">
+                <h1 className="text-right">{today}</h1>
             </div>
             <div className="row ml-auto time">
                 <h1>{time}</h1>{/*we use {time} to insert the dynamic value of 'time' into this h1*/}
